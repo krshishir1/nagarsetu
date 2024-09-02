8a016ea96a3ac467f0e08718a4cdab576238bb12
Shishir <shishir3324kumar@gmail.com>
2024-09-02 23:53:54 +0530

    project started with vite and tailwind installation

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h2>
+          Nagar Setu Project
+        </h2>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ readme.md
@@ -0,0 +1,6 @@
+
+## Nagar Setu (SIH Project)
+
+This project aims to serve a platform for Inter-departmental cooperation (at city level) in Indian Cities, for sharing data & resources, unified phasing, planning and implementation of projects.
+
+
